43d449657618653a77e0f77b237d43d31a655678
allisonkydy <allisnelly@gmail.com>
2019-09-06 10:14:17 -0500

    refactored app into class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
