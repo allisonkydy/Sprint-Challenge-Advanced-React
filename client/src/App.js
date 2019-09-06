43a7abeff46119dcfd8291a6fd6dfb7673a3d611
allisonkydy <allisnelly@gmail.com>
2019-09-06 10:21:37 -0500

    fetching player data from local server

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,23 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      playerData: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(res => {
+        // console.log(res);
+        this.setState({ playerData: res });
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
